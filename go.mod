cc729c74bb350c3b0dbe49e5a90392a4e04d0a0b
ItzikSdev <itziksavaia@gmail.com>
2024-07-10 11:29:43 +0300

    upload file function

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/ItzikSdev/auto-cv
+
+go 1.22.3
+
+require (
+	github.com/dslipak/pdf v0.0.2 // indirect
+	github.com/nguyenthenguyen/docx v0.0.0-20230621112118-9c8e795a11db // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
+
+	"github.com/dslipak/pdf"
+	"github.com/nguyenthenguyen/docx"
+)
+
+type FileInfo struct {
+	Name string `json:"name"`
+	Type string `json:"type"`
+	Size int64  `json:"size"`
+	Text string `json:"text"`
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("File Upload Endpoint Hit")
+	r.ParseMultipartForm(10 << 20)
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+
+	fileType := filepath.Ext(handler.Filename)
+	if fileType == ".pdf" {
+		content, err := pdf.NewReader(file, handler.Size)
+		if err != nil {
+			panic(err)
+		}
+		plainText, err := content.GetPlainText()
+		if err != nil {
+			http.Error(w, "error", http.StatusInternalServerError)
+		}
+		text, err := io.ReadAll(plainText)
+		if err != nil {
+			http.Error(w, "error", http.StatusInternalServerError)
+		}
+		f := FileInfo{
+			Name: handler.Filename,
+			Type: fileType[1:],
+			Size: handler.Size,
+			Text: string(text),
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(f); err != nil {
+			http.Error(w, "error encoding response", http.StatusInternalServerError)
+		}
+		return
+	}
+	if fileType == ".docx" {
+		content, err := docx.ReadDocxFromMemory(file, handler.Size)
+		if err != nil {
+			panic(err)
+		}
+		f := FileInfo{
+			Name: handler.Filename,
+			Type: fileType[1:],
+			Size: handler.Size,
+			Text: content.Editable().GetContent(),
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(f); err != nil {
+			http.Error(w, "error encoding response", http.StatusInternalServerError)
+		}
+		return
+	}
+
+	http.Error(w, "unsupported file type", http.StatusBadRequest)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/upload", uploadFile)
+	http.ListenAndServe(":8080", nil)
+}
+
+func main() {
+	setupRoutes()
+}
